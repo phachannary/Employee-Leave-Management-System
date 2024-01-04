33b6127053f14ffd49964c8ade7701da6aff964c
Mengheang Pho <Mengheang.Pho@pnc.lan>
2024-01-04 22:57:06 +0700

    add menu bar

--- src/components/HeaderComponent/index.js
+++ src/components/HeaderComponent/index.js
@@ -3,7 +3,6 @@ import "./header-component.css";
 class Header extends React.Component {
   render() {
     let content = this.props.parentToChild;
-    console.log(content);
     return (
       <div class="header-container">
         <h5>{content}</h5>
