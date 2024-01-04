04e8d71e0c06b5de56963bf973339eb4f6ac1f53
Chanthy Tha <chanthy.tha@cksntransport.com>
2024-01-05 01:56:19 +0700

    add leave history

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import LeaveDetail from "./pages/LeaveDetail";
 import Dashboard from "./pages/dashboard";
+import LeaveManagement from './pages/LeaveManagement';
 
 
 function App() {
@@ -10,8 +11,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route path='/' element={<Dashboard />} />
           <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/leave-management' element={<LeaveManagement/>} />
           <Route path='/leave-detail/:leaveID' element={<LeaveDetail/>} />
         </Routes>
       </BrowserRouter>
